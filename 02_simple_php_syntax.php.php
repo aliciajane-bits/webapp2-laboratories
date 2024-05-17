f5933a0da65c2d1ac7db97c26944ea9206dcca6c
Alicia Jane Medina <149754640+aliciajane-bits@users.noreply.github.com>
2024-05-17 21:55:14 +0800

    Add files via upload

---
+++ 02_simple_php_syntax.php.php
@@ -0,0 +1,52 @@
+<?php
+//Number 1:
+$name = "Alicia";
+$age = 20;
+$address = "Mangaldan, Pangasinan";
+$hobbies = "drawing and crocheting";
+$pet_peeve = "hypocrite";
+
+echo $name . PHP_EOL; 
+echo $age . PHP_EOL;
+echo $address . PHP_EOL;
+echo $hobbies . PHP_EOL;
+echo $pet_peeve . PHP_EOL . PHP_EOL;
+
+//Number 2:
+$name = "Alicia";
+$age = 20;
+$address = "Mangaldan, Pangasinan";
+// $hobbies = "drawing and crocheting";
+// $pet_peeve = "hypocrite";
+
+
+//Number 3:
+const NAME = "ALICIA";
+const AGE = 20;
+const ADDRESS = "MANGALDAN, PANGASINAN";
+const HOBBIES = "DRAWING AND CROCHETING";
+const PET_PEEVE = "HYPOCRITE";
+
+echo NAME . PHP_EOL; 
+echo AGE . PHP_EOL;
+echo ADDRESS . PHP_EOL;
+echo HOBBIES . PHP_EOL;
+echo PET_PEEVE . PHP_EOL . PHP_EOL;
+
+//Number 4:
+var_dump (NAME, AGE, ADDRESS, HOBBIES, PET_PEEVE);
+
+//Number 5:
+$name = "Alicia";
+$age = 20;
+$address = "Mangaldan, Pangasinan";
+$hobbies = "drawing and crocheting";
+$pet_peeve = "hypocrite"; 
+/* WHY DO I HAVE PET PEEVE ON HYPOCRITES
+I really really dislike hypocrites.
+It is because when you are lecturing someone about their bad habits or bad behaviour,
+you as someone lecturing about that bad habit must not have that bad habit yourself.
+I just find it really iritating especially when someone blunders of doing good things and changing your bad habits,
+but they themselves don't do that.
+Basically, how can I have the urge to change my bad habits and bad behaviour when the person saying it to me don't even do it.
+In general, hyprocrite people really gives me the ick.*/
